0da6fc357bb28b4a982194373fb40e7941b0879d
Vignesh <vignesh936069@gmail.com>
2026-07-26 20:07:12 +0530

    Time: 4 ms (16.58%) | Memory: 43.6 MB (20.83%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -2,7 +2,6 @@ class Solution {
     public String truncateSentence(String s, int k) {
         String[] arr = s.split(" ");
         String vicky = "";
-
         for (int i = 0; i < k; i++) {
             if (i == 0) {
                 vicky += arr[i];
@@ -10,7 +9,6 @@ class Solution {
                 vicky += " " + arr[i];
             }
         }
-
         return vicky;
     }
 }
