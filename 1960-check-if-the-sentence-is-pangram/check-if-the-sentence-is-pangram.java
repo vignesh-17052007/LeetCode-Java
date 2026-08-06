3f3081bcb2f3f86c63a1ae5dfe401f12d8e75a16
Vignesh S <vignesh936069@gmail.com>
2026-08-06 19:18:20 +0530

    Time: 4 ms (20.07%) | Memory: 43.1 MB (34.00%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,16 +1,14 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean checkIfPangram(String sentence) {
 
-        boolean[] seen = new boolean[26];
-        int sum = 0;
+        HashSet<Character> set = new HashSet<>();
+
         for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
-            if (!seen[ch - 'a']) {
-                seen[ch - 'a'] = true;
-                sum += ch;  
-            }
+            set.add(sentence.charAt(i));
         }
 
-        return sum == 2847;
+        return set.size() == 26;
     }
 }
