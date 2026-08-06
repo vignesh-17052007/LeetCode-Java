0b9ca4e33f29d4aabeead03b702a79eb7be73b60
Vignesh S <vignesh936069@gmail.com>
2026-08-06 19:09:29 +0530

    Time: 2 ms (63.67%) | Memory: 42.7 MB (84.74%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -3,14 +3,11 @@ class Solution {
 
         boolean[] seen = new boolean[26];
         int sum = 0;
-
-        // Remove duplicates
         for (int i = 0; i < sentence.length(); i++) {
             char ch = sentence.charAt(i);
-
             if (!seen[ch - 'a']) {
                 seen[ch - 'a'] = true;
-                sum += ch;   // Add ASCII value
+                sum += ch;  
             }
         }
 
